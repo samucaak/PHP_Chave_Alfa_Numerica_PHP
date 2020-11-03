92b80eba8cc7737c9aa75b6a9b51556c82a76db6
samucaak <66152838+samucaak@users.noreply.github.com>
2020-11-03 14:16:17 -0300

    03/11/2020 v0.4

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'samuel/chave_alfa_numerica',
+  ),
+  'versions' => 
+  array (
+    'samuel/chave_alfa_numerica' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
