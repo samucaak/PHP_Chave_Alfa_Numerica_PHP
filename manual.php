40592bd4e2e20990b5d8a720d3607c4276baed3b
samucaak <66152838+samucaak@users.noreply.github.com>
2020-11-03 14:06:31 -0300

    03/11/2020 V0.3

--- manual.php
+++ manual.php
@@ -1,15 +1,24 @@
 <?php
-require_once("chave.php");
-$chave = new Chave;
+require_once("chave.php");//adicionando a class chave ao arquivo
+$chave = new Chave;//instanciando a clsse chave
 
+
+//O parametro passado para a funçao 
+//é a quantidade de caracteres que deseja na para a chave.
+
+        //CHAVE ALFA NUMÉRICA
 $alfanum = $chave->chaveAlfaNumerica(60);
 echo "CHAVE ALFA NUMERICA: $alfanum";
 //________________________________________
 
+
+          //CHAVE ALFA
 $alfa = $chave->chaveAlfa(50);
 echo "<hr><br> CHAVE ALFA: $alfa";
 //________________________________________
 
+
+            //CHAVE NUMÉRICA
 $num = $chave->chaveNumerica(50);
 echo "<hr><br> CHAVE NUMERICA: $num";
 //________________________________________
